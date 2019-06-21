02d486d13f37e594082736cdae44a363ef32acd6
Andrew English <aindriuinglis@gmail.com>
2019-06-21 20:37:09 +0100

    monochrome commit

---
+++ html-Personal Site/scripts/script.js
@@ -0,0 +1,12 @@
+$(function(){
+
+    $(".hello").mouseenter(function(){
+
+        $(".hello").animate({fontSize:'50px'}, "fast");
+        $(".hello").animate({fontSize: '30px'},"slow");
+
+    });
+
+    $(".")
+
+});
