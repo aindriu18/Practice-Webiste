92de93ff44da1139c9d2a1b1b25de75819997696
Andrew English <aindriuinglis@gmail.com>
2019-06-24 22:47:29 +0100

    java push

--- html-Personal Site/scripts/script.js
+++ html-Personal Site/scripts/script.js
@@ -1,4 +1,4 @@
-$(function(){
+$(function() {
 
     $(".hello").mouseenter(function(){
 
@@ -7,6 +7,36 @@ $(function(){
 
     });
 
-    $(".")
+    $(document).ready(function() {
+      function topCloudLeft() {
+          $(".top-cloud").animate({left: "+=500"}, 20000, "swing", topCloudRight);
+      }
+      function topCloudRight() {
+          $(".top-cloud").animate({left: "-=500"}, 20000, "swing", topCloudLeft);
+      }
+      topCloudRight();
+    });
+
+    $(document).ready(function() {
+        function bottomCloudLeft() {
+          $(".bottom-cloud").animate({left: "+=300"}, 20000, "swing", bottomCloudRight);
+      }
+      function bottomCloudRight() {
+          $(".bottom-cloud").animate({left: "-=300"}, 20000, "swing", bottomCloudLeft);
+      }
+      bottomCloudRight();
+    });
+    
+     
 
 });
+
+/* gives the user the opportunity to toggle between the navbar with (.icon) */
+function menu() {
+  var x = document.getElementById("myNavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
