56750eeb600ba7b3da244d345a942a42d0ae5be7
Piotr Gospodarczyk <fedpy@protonmail.com>
2019-07-05 12:19:43 +0100

    JavaScript Refactored

---
+++ scripts/education.js
@@ -0,0 +1,181 @@
+jQuery(document).ready(function($) {
+
+  /*
+  Software
+  */
+  $.ajax({
+    url: "./data/software.csv",
+    type: "GET",
+    dataType: "text"
+  })
+  .done(function(responeData) {
+
+    var jsonData = $.csv.toObjects(responeData);
+
+    for (i = 0; i < jsonData.length; ++i) {
+
+      var content =
+      "<tr>" +
+        "<td>" + jsonData[i]["Course Code"] + "</td>" +
+        "<td>" + jsonData[i]["Course Module"] + "</td>" +
+        "<td>" + jsonData[i]["Grade"] + "</td>" +
+      "</tr>";
+      $("#software").append(content);
+
+    };
+
+  })
+  .fail(function(responseError) {
+
+    var errorContent =
+    "<tr>" +
+      "<td colspan='4'>We are sorry, an error occured. Download the file <a href='/data/software.csv'>here</a>.</td>" +
+    "</tr>";
+    $("#software").append(errorContent);
+
+  });
+
+  /*
+    Masters
+    */
+  $.ajax({
+    url: "./data/masters.csv",
+    type: "GET",
+    dataType: "text"
+  })
+  .done(function(responeData) {
+
+    var jsonData = $.csv.toObjects(responeData);
+
+    for (i = 0; i < jsonData.length; ++i) {
+
+    var content =
+    "<tr>" +
+      "<td>" + jsonData[i]["Course Code"] + "</td>" +
+      "<td>" + jsonData[i]["Course Module"] + "</td>" +
+      "<td>" + jsonData[i]["Grade"] + "</td>" +
+      "<td>" + jsonData[i]["GPA"] + "</td>" +
+    "</tr>";
+    $("#masters").append(content);
+
+    };
+
+  })
+  .fail(function(responseError) {
+
+    var errorContent =
+    "<tr>" +
+      "<td colspan='4'>We are sorry, an error occured. Download the file <a href='/data/masters.csv'>here</a>.</td>" +
+    "</tr>";
+    $("#masters").append(errorContent);
+
+  });
+
+  /*
+  Year 3
+  */
+  $.ajax({
+    url: "./data/year_3.csv",
+    type: "GET",
+    dataType: "text"
+  })
+  .done(function(responeData) {
+
+    var jsonData = $.csv.toObjects(responeData);
+
+    for (i = 0; i < jsonData.length; ++i) {
+
+    var content =
+    "<tr>" +
+      "<td>" + jsonData[i]["Course Code"] + "</td>" +
+      "<td>" + jsonData[i]["Course Module"] + "</td>" +
+      "<td>" + jsonData[i]["Grade"] + "</td>" +
+      "<td>" + jsonData[i]["GPA"] + "</td>" +
+    "</tr>";
+    $("#year3").append(content);
+
+    };
+
+  })
+  .fail(function(responseError) {
+
+    var errorContent =
+    "<tr>" +
+      "<td colspan='4'>We are sorry, an error occured. Download the file <a href='/data/year_3.csv'>here</a>.</td>" +
+    "</tr>";
+    $("#year3").append(errorContent);
+
+  });
+
+  /*
+  Year 2
+  */
+  $.ajax({
+    url: "./data/year_2.csv",
+    type: "GET",
+    dataType: "text"
+  })
+  .done(function(responeData) {
+
+    var jsonData = $.csv.toObjects(responeData);
+
+    for (i = 0; i < jsonData.length; ++i) {
+
+    var content =
+    "<tr>" +
+      "<td>" + jsonData[i]["Course Code"] + "</td>" +
+      "<td>" + jsonData[i]["Course Module"] + "</td>" +
+      "<td>" + jsonData[i]["Grade"] + "</td>" +
+      "<td>" + jsonData[i]["GPA"] + "</td>" +
+    "</tr>";
+    $("#year2").append(content);
+
+    };
+
+  })
+  .fail(function(responseError) {
+
+    var errorContent =
+    "<tr>" +
+      "<td colspan='4'>We are sorry, an error occured. Download the file <a href='/data/year_2.csv'>here</a>.</td>" +
+    "</tr>";
+    $("#year2").append(errorContent);
+
+  });
+
+  /*
+  Year 1
+  */
+  $.ajax({
+    url: "./data/year_1.csv",
+    type: "GET",
+    dataType: "text"
+  })
+  .done(function(responeData) {
+
+    var jsonData = $.csv.toObjects(responeData);
+    for (i = 0; i < jsonData.length; ++i) {
+
+    var content = 
+    "<tr>" +
+      "<td>" + jsonData[i]["Course Code"] + "</td>" +
+      "<td>" + jsonData[i]["Course Module"] + "</td>" +
+      "<td>" + jsonData[i]["Grade"] + "</td>" +
+      "<td>" + jsonData[i]["GPA"] + "</td>" +
+    "</tr>";
+    $("#year1").append(content);
+
+    };
+
+  })
+  .fail(function(responseError) {
+
+    var errorContent =
+    "<tr>" +
+      "<td colspan='4'>We are sorry, an error occured. Download the file <a href='/data/year_1.csv'>here</a>.</td>" +
+    "</tr>";
+    $("#year1").append(errorContent);
+
+  });
+
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,246 +1,60 @@
-$(function() {
+jQuery(document).ready(function($) {
 
-  /*$("#contact-form").parsley(); */
+  // Initialize the cloud animation effects
+  topCloudRight();
+  bottomCloudRight();
 
-    $(".myBook").mouseenter(function(){
+  function topCloudLeft() {
+    $(".top-cloud").animate({
+      left: "+=500"
+    }, 20000, "swing", topCloudRight);
+  };
 
-        $(".myBook").animate({fontSize:'50px'}, "fast");
-        $(".myBook").animate({fontSize: '30px'},"slow");
+  function topCloudRight() {
+    $(".top-cloud").animate({
+      left: "-=500"
+    }, 20000, "swing", topCloudLeft);
+  };
 
-    });
+  function bottomCloudLeft() {
+    $(".bottom-cloud").animate({
+      left: "+=300"
+    }, 20000, "swing", bottomCloudRight);
+  };
 
-      /* from its starting position, the cloud gradually moves left before coming back to its original position*/
-    $(document).ready(function() {
-      function topCloudLeft() {
-          $(".top-cloud").animate({left: "+=500"}, 20000, "swing", topCloudRight);
-      }
-      function topCloudRight() {
-          $(".top-cloud").animate({left: "-=500"}, 20000, "swing", topCloudLeft);
-      }
-      topCloudRight();
-    });
+  function bottomCloudRight() {
+    $(".bottom-cloud").animate({
+      left: "-=300"
+    }, 20000, "swing", bottomCloudLeft);
+  };
 
-      /* from its starting position, the cloud gradually moves right before coming back to its original position*/
-    $(document).ready(function() {
-        function bottomCloudLeft() {
-          $(".bottom-cloud").animate({left: "+=300"}, 20000, "swing", bottomCloudRight);
-      }
-      function bottomCloudRight() {
-          $(".bottom-cloud").animate({left: "-=300"}, 20000, "swing", bottomCloudLeft);
-      }
-      bottomCloudRight();
-    });
+  /* facilitates responsive viewing for the navbar on mobile devices */
+  $("#myNavbar").click(function(event) {
 
+    menu();
 
-
-/* ---- pulling education details from a .csv file 
-  that represents each year of education and the 
-  corresponding modules and results*/
-
-  $.ajax({
-    type: "GET",
-    url: "./software.csv",
-    dataType: "text",
-    success: function(data) {
-      dataSoftware(data);
-
-    }
   });
 
-    $.ajax({
-      type: "GET",
-      url: "./Masters.csv",
-      dataType: "text",
-      success: function(data) {
-        dataMasters(data);
+  function menu() {
 
-      }
-    });
+    var x = document.getElementById("myNavbar");
 
-    $.ajax({
-      type: "GET",
-      url: "./Year-3.csv",
-      dataType: "text",
-      success: function(data) {
-        dataYear3(data);
+    if (x.className == "navbar") {
 
-      }
-    });
+      x.className += " responsive";
 
-    $.ajax({
-      type: "GET",
-      url: "./Year-2.csv",
-      dataType: "text",
-      success: function(data) {
-        dataYear2(data);
+    } else {
 
-      }
-    });
+      x.className = "navbar";
 
-    $.ajax({
-      type: "GET",
-      url: "./Year-1.csv",
-      dataType: "text",
-      success: function(data) {
-        dataYear1(data);
+    };
 
-      }
-    });
+  };
 
+  $("#contact-form > input[type='submit']").click(function(event) {
 
-    // Software apprenticeship data
+      $("#contact-form").parsley();
 
-    function dataSoftware(data) {
-      
+  });
 
-      var jsonData = $.csv.toObjects(data);
-      console.table(jsonData);
-
-      $("#software").append();
-      for (i = 0; i < jsonData.length; ++i) {
-        //Looping through data to add data from csv file
-
-        var content = "<tr>" +
-          "<td>" + jsonData[i]["Course Code"] + "</td>" +
-          "<td>" + jsonData[i]["Course Module"] + "</td>" +
-          "<td>" + jsonData[i]["Grade"] + "</td>" +
-        "</tr>";
-
-        $("#software").append(content);
-
-      }
-    }
-
-    // -- masters data
-    function dataMasters(data) {
-      
-
-      var jsonData = $.csv.toObjects(data);
-      console.table(jsonData);
-
- 
-
-
-      $("#masters").append();
-      for (i = 0; i < jsonData.length; ++i) {
-        //Looping through data to add data from csv file
-
-        var content = "<tr>" +
-          "<td>" + jsonData[i]["Course Code"] + "</td>" +
-          "<td>" + jsonData[i]["Course Module"] + "</td>" +
-          "<td>" + jsonData[i]["Grade"] + "</td>" +
-          "<td>" + jsonData[i]["GPA"] + "</td>";
-        "</tr>";
-
-        $("#masters").append(content);
-
-      }
-    }
-
-    
-    // ----Year 3 data
-    function dataYear3(data) {
-    
-
-      var jsonData = $.csv.toObjects(data);
-      console.table(jsonData);
-
-      $("#year3").append();
-      for (i = 0; i < jsonData.length; ++i) {
-        //Looping through data to add data from csv file
-
-        var content = "<tr>" +
-          "<td>" + jsonData[i]["Course Code"] + "</td>" +
-          "<td>" + jsonData[i]["Course Module"] + "</td>" +
-          "<td>" + jsonData[i]["Grade"] + "</td>" +
-          "<td>" + jsonData[i]["GPA"] + "</td>";
-        "</tr>";
-
-        $("#year3").append(content);
-
-      }
-    }
-
-    // ----Year 2 data
-    function dataYear2(data) {
-    
-
-      var jsonData = $.csv.toObjects(data);
-      console.table(jsonData);
-
-      $("#year2").append();
-      for (i = 0; i < jsonData.length; ++i) {
-        //Looping through data to add data from csv file
-
-        var content = "<tr>" +
-          "<td>" + jsonData[i]["Course Code"] + "</td>" +
-          "<td>" + jsonData[i]["Course Module"] + "</td>" +
-          "<td>" + jsonData[i]["Grade"] + "</td>" +
-          "<td>" + jsonData[i]["GPA"] + "</td>";
-        "</tr>";
-
-        $("#year2").append(content);
-
-      }
-    }
-
-    // ----Year 1 data
-    function dataYear1(data) {
-    
-
-      var jsonData = $.csv.toObjects(data);
-      console.table(jsonData);
-
-      $("#year1").append();
-      for (i = 0; i < jsonData.length; ++i) {
-        //Looping through data to add data from csv file
-
-        var content = "<tr>" +
-          "<td>" + jsonData[i]["Course Code"] + "</td>" +
-          "<td>" + jsonData[i]["Course Module"] + "</td>" +
-          "<td>" + jsonData[i]["Grade"] + "</td>" +
-          "<td>" + jsonData[i]["GPA"] + "</td>";
-        "</tr>";
-
-        $("#year1").append(content);
-
-      }
-    }
-
-});
-
-/* facilitates responsive viewing for the navbar on mobile devices */
-function menu() {
-  var x = document.getElementById("myNavbar");
-  if (x.className === "navbar") {
-    x.className += " responsive";
-  } else {
-    x.className = "navbar";
-  }
-}
-
-
-/* gives data table structure and definition. Achieved through datatables CDN on education.html  */
-
-$(document).ready( function () {
-  $('#software').DataTable();
-} );
-
-$(document).ready( function () {
-  $('#masters').DataTable();
-} );
-
-$(document).ready( function () {
-  $('#year3').DataTable();
-} );
-
-$(document).ready( function () {
-  $('#year2').DataTable();
-} );
-
-$(document).ready( function () {
-  $('#year1').DataTable();
-} );
-
-
-$("#contact-form").parsley();
+});  // End of jQuery Ready Document
